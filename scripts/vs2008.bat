17f64c25a2fa908e732e60a6d0f964ec1ac44add
U-t500\tade <tade@t500.(none)>
2013-03-02 17:28:54 +0100

    visual studio runner, just edit BOOST_ROOT and run

---
+++ scripts/vs2008.bat
@@ -0,0 +1,13 @@
+
+set BOOST_ROOT=d:\work\deps-vs2008\boost_1_43_0\
+set BUILD=..\build-vs2008\
+
+mkdir %BUILD%
+
+pushd %BUILD%
+cmake.exe -DBOOST_ROOT=%BOOST_ROOT% ..\src\
+popd
+
+set PATH=%CD%\..\3rd_party\libusb-win32-bin-1.2.6.0\bin\x86\;%PATH%
+
+start "c:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE\devenv.exe" "%BUILD%\antpm.sln"
