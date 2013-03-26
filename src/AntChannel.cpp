472aafa9efc299a046a9b838d2b23c44fdade87f
RALOVICH, Kristóf <tade60@freemail.hu>
2013-03-26 17:52:10 +0100

    reindent
    no functional changes

--- src/AntChannel.cpp
+++ src/AntChannel.cpp
@@ -195,105 +195,106 @@ AntBCastListener::waitForBCast(AntMessage& bcast, const size_t timeout_ms)
 
 void
 AntBurstListener::onMsg(AntMessage& m)
+{
+  boost::unique_lock<boost::mutex> lock(m_mtxResp);
+  if(match(m))
   {
-    boost::unique_lock<boost::mutex> lock(m_mtxResp);
-    if(match(m))
-    {
-      bursts.push_back(m);
-      m_cndResp.notify_all();
-    }
+    bursts.push_back(m);
+    m_cndResp.notify_all();
   }
+}
 
 
 bool
 AntBurstListener::match(AntMessage& other) const
-  {
-    const M_ANT_Burst* burst(reinterpret_cast<const M_ANT_Burst*>(other.getPayloadRef()));
-    return other.getMsgId()==MESG_BURST_DATA_ID
-        && burst->chan==owner.chan;
-  }
+{
+  const M_ANT_Burst* burst(reinterpret_cast<const M_ANT_Burst*>(other.getPayloadRef()));
+  return other.getMsgId()==MESG_BURST_DATA_ID
+    && burst->chan==owner.chan;
+}
 
 
 bool
 AntBurstListener::waitForBursts(std::list<AntMessage>& bs, const size_t timeout_ms)
+{
+  bs.clear();
+  boost::unique_lock<boost::mutex> lock(m_mtxResp);
+  if(!bursts.empty()) // some had already arrived
   {
-    bs.clear();
-    boost::unique_lock<boost::mutex> lock(m_mtxResp);
-    if(!bursts.empty()) // some had already arrived
-    {
-      std::swap(bs, bursts);
-      return true;
-    }
-    // TODO: handle arrival of event:EVENT_RX_FAIL
-    if(!m_cndResp.timed_wait(lock, boost::posix_time::milliseconds(timeout_ms)))
-      return false;
-    assert(!bursts.empty());
     std::swap(bs, bursts);
     return true;
   }
+  // TODO: handle arrival of event:EVENT_RX_FAIL
+  if(!m_cndResp.timed_wait(lock, boost::posix_time::milliseconds(timeout_ms)))
+    return false;
+  assert(!bursts.empty());
+  std::swap(bs, bursts);
+  return true;
+}
+
 
 bool
 AntBurstListener::collectBurst(std::vector<uint8_t>& burstData, const size_t timeout_ms)
-  {
-    boost::posix_time::ptime start = boost::posix_time::microsec_clock::local_time();
-    boost::posix_time::ptime end = start+boost::posix_time::milliseconds(timeout_ms);
+{
+  boost::posix_time::ptime start = boost::posix_time::microsec_clock::local_time();
+  boost::posix_time::ptime end = start+boost::posix_time::milliseconds(timeout_ms);
 
-    boost::posix_time::ptime after=start;
-    boost::posix_time::time_duration remaining = end-after;
-    uchar expectedSeq=0;
-    bool found=false;
-    bool lastFound=false;
-    while(remaining>boost::posix_time::time_duration(boost::posix_time::milliseconds(0)))
+  boost::posix_time::ptime after=start;
+  boost::posix_time::time_duration remaining = end-after;
+  uchar expectedSeq=0;
+  bool found=false;
+  bool lastFound=false;
+  while(remaining>boost::posix_time::time_duration(boost::posix_time::milliseconds(0)))
+  {
+    std::list<AntMessage> msgs;
+    size_t left_ms=static_cast<size_t>(remaining.total_milliseconds());
+    //printf("msg wait ms=%d\n",  int(left_ms));
+    CHECK_RETURN_FALSE(waitForBursts(msgs, left_ms));
+    after = boost::posix_time::microsec_clock::local_time();
+    remaining = end-after;
+    for(std::list<AntMessage>::iterator i = msgs.begin(); i != msgs.end(); i++)
     {
-      std::list<AntMessage> msgs;
-      size_t left_ms=static_cast<size_t>(remaining.total_milliseconds());
-      //printf("msg wait ms=%d\n",  int(left_ms));
-      CHECK_RETURN_FALSE(waitForBursts(msgs, left_ms));
-      after = boost::posix_time::microsec_clock::local_time();
-      remaining = end-after;
-      for(std::list<AntMessage>::iterator i = msgs.begin(); i != msgs.end(); i++)
-      {
-        AntMessage& repl(*i);
-        CHECK_RETURN_FALSE(repl.getLenPayload()==9);
-        const M_ANT_Burst* burst(reinterpret_cast<const M_ANT_Burst*>(repl.getPayloadRef()));
-        //if(burst->chan != chan) // this check is a duplicate
-        //{
-        //  //++i;j++;
-        //  continue;
-        //}
-        //LOG_VAR2((int)burst->seq, (int)expectedSeq);
-        CHECK_RETURN_FALSE(burst->seq == expectedSeq);
-        ++expectedSeq;
-        if(expectedSeq == 4)
-          expectedSeq = 1;
-        found = true;
-        std::vector<uchar> crtBurst(repl.getPayload());
-        //std::string desc=burst->toString();
-        //printf("reading %d bytes of BURST:%s\n", int(crtBurst.size()), desc.c_str());
-        burstData.insert(burstData.end(), crtBurst.begin()+1,crtBurst.end());
-        assert(burstData.size()%8==0);
-        lastFound = burst->isLast();
-        //i=m_rpackQueue.erase(i);j++;
-        //if(burst->isLast())
-        //{
-        //  lastFound = true;
-        //  //break;
-        //}
-        //LOG_VAR(lastFound);
-      }
-      if(lastFound)
-      {
-        //LOG_VAR(lastFound);
-        break;
-      }
+      AntMessage& repl(*i);
+      CHECK_RETURN_FALSE(repl.getLenPayload()==9);
+      const M_ANT_Burst* burst(reinterpret_cast<const M_ANT_Burst*>(repl.getPayloadRef()));
+      //if(burst->chan != chan) // this check is a duplicate
+      //{
+      //  //++i;j++;
+      //  continue;
+      //}
+      //LOG_VAR2((int)burst->seq, (int)expectedSeq);
+      CHECK_RETURN_FALSE(burst->seq == expectedSeq);
+      ++expectedSeq;
+      if(expectedSeq == 4)
+        expectedSeq = 1;
+      found = true;
+      std::vector<uchar> crtBurst(repl.getPayload());
+      //std::string desc=burst->toString();
+      //printf("reading %d bytes of BURST:%s\n", int(crtBurst.size()), desc.c_str());
+      burstData.insert(burstData.end(), crtBurst.begin()+1,crtBurst.end());
+      assert(burstData.size()%8==0);
+      lastFound = burst->isLast();
+      //i=m_rpackQueue.erase(i);j++;
+      //if(burst->isLast())
+      //{
+      //  lastFound = true;
+      //  //break;
+      //}
+      //LOG_VAR(lastFound);
     }
-    if(!found || !lastFound)
+    if(lastFound)
     {
-      lprintf(LOG_ERR, "couldn't reconstruct burst data transmission before timeout\n"); fflush(stdout);
-      return false;
+      //LOG_VAR(lastFound);
+      break;
     }
-    lprintf(LOG_INF, "collectBurst: %d bytes\n", int(burstData.size()));
-    return true;
   }
+  if(!found || !lastFound)
+  {
+    lprintf(LOG_ERR, "couldn't reconstruct burst data transmission before timeout\n"); fflush(stdout);
+    return false;
+  }
+  lprintf(LOG_INF, "collectBurst: %d bytes\n", int(burstData.size()));
+  return true;
+}
 
 }
