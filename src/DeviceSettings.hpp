c2c481daa9f85c0ea0fd7caae28238a8786d2d2a
RALOVICH, Kristóf <tade60@freemail.hu>
2013-06-23 23:04:01 +0200

    first stub for centralized per-device setting

---
+++ src/DeviceSettings.hpp
@@ -0,0 +1,24 @@
+// -*- mode: c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2; coding: utf-8-unix -*-
+// ***** BEGIN LICENSE BLOCK *****
+////////////////////////////////////////////////////////////////////
+//                                                                //
+// Copyright (c) 2011-2013 RALOVICH, Kristóf                      //
+//                                                                //
+// This program is free software; you can redistribute it and/or  //
+// modify it under the terms of the GNU General Public License    //
+// version 2 as published by the Free Software Foundation.        //
+//                                                                //
+////////////////////////////////////////////////////////////////////
+// ***** END LICENSE BLOCK *****
+
+
+namespace antpm {
+
+struct DeviceSettings
+{
+  int         MaxFileDownloads;
+  std::time_t LastUserProfileTime;
+  std::time_t LastTransferredTime;
+};
+
+}
