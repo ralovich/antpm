13efb199b72b1469b35d73fab7e003149c03130d
RALOVICH, Kristof <tade60@freemail.hu>
2014-01-06 00:08:47 +0100

    add BSWAP_64 macro

--- src/common.hpp
+++ src/common.hpp
@@ -69,6 +69,15 @@ writeUInt64(const unsigned int clientSN, const uint64_t& ui);
 uint64_t
 SwapDWord(uint64_t a);
 
+#define	BSWAP_64(x)     (((uint64_t)(x) << 56) | \
+                        (((uint64_t)(x) << 40) & 0xff000000000000ULL) | \
+                        (((uint64_t)(x) << 24) & 0xff0000000000ULL) | \
+                        (((uint64_t)(x) << 8)  & 0xff00000000ULL) | \
+                        (((uint64_t)(x) >> 8)  & 0xff000000ULL) | \
+                        (((uint64_t)(x) >> 24) & 0xff0000ULL) | \
+                        (((uint64_t)(x) >> 40) & 0xff00ULL) | \
+                        ((uint64_t)(x)  >> 56))
+
 std::vector<unsigned char>
 readFile(const char* fileName);
 
