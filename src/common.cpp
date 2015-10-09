9908aad9efca2fd0a73e18a870d137076e2f4739
RALOVICH, Kristof <tade60@freemail.hu>
2015-10-09 23:02:00 +0200

    don't use __DATE__ for reproducible builds

--- src/common.cpp
+++ src/common.cpp
@@ -307,7 +307,7 @@ getVersionString()
 {
   return std::string("") + APP_NAME
       + " v" + std::string(BOOST_STRINGIZE(ANTPM_VERSION))
-      + " built " __DATE__ " " + __TIME__ + "" //+#ANTPM_SERIAL_IMPL
+      + " built "
       + " under "
 #ifdef __linux__
 # ifdef __LP64__
