c5b42de1b8ede6fc028f83e2cd2c665aec20f2c8
RALOVICH, Kristof <tade60@freemail.hu>
2020-04-08 22:08:13 +0200

    cmake: use pkg-config to find libusb-1.0 under Linux

--- scripts/travis_windows.cmd
+++ scripts/travis_windows.cmd
@@ -3,6 +3,6 @@
 
 echo %PATH%
 cd
-ipconfig /all
+choco install boost-msvc-14.1
 cd cmake-build
-cmake ../src -DCMAKE_BUILD_TYPE=Release -DUSE_BOOST_TEST=TRUE -DCMAKE_INSTALL_PREFIX=/usr -DUSE_BOOST_STATIC_LINK=FALSE
+cmake ../src -DCMAKE_BUILD_TYPE=Release -DUSE_BOOST_TEST=TRUE -DCMAKE_INSTALL_PREFIX="c:\opt\" -DUSE_BOOST_STATIC_LINK=FALSE
