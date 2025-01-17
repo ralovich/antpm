84b92f7d7309f6fc28c6e9091b863d008859fb81
Kristof Ralovich <tade60@freemail.hu>
2025-01-17 21:38:30 +0100

    search for .lib too

--- src/cmake/Findlibusb-1.0.cmake
+++ src/cmake/Findlibusb-1.0.cmake
@@ -49,7 +49,7 @@ if (LIBUSB_1_LIBRARIES AND LIBUSB_1_INCLUDE_DIRS)
 else (LIBUSB_1_LIBRARIES AND LIBUSB_1_INCLUDE_DIRS)
   find_path(LIBUSB_1_INCLUDE_DIR
     NAMES
-	libusb.h
+       libusb.h
     PATHS
       /usr/include
       /usr/local/include
@@ -63,11 +63,13 @@ else (LIBUSB_1_LIBRARIES AND LIBUSB_1_INCLUDE_DIRS)
   find_library(LIBUSB_1_LIBRARY
     NAMES
       usb-1.0
+      libusb-1.0.lib
     PATHS
       /usr/lib
       /usr/local/lib
       /opt/local/lib
       /sw/lib
+      ${LIBUSB_ROOT}/lib
   )
 
   set(LIBUSB_1_INCLUDE_DIRS
